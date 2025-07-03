f62e64976eeba20d4ff564c87a174a0dd603f093
Ayush <ayushjadaun6@gmail.com>
2025-07-03 13:01:33 +0530

    refractor backend in uv

--- backend/agent.py
+++ backend/agent.py
@@ -1,4 +1,4 @@
-from dotenv import load_dotenv # type: ignore
+from dotenv import load_dotenv
 
 from livekit import agents # type: ignore
 from livekit.agents import AgentSession, Agent, RoomInputOptions # type: ignore
@@ -12,6 +12,7 @@ from livekit.plugins import ( # type: ignore
 from livekit.plugins.turn_detector.multilingual import MultilingualModel # type: ignore
 
 load_dotenv()
+load_dotenv()
 
 class Assistant(Agent):
     def __init__(self) -> None:
@@ -20,6 +21,8 @@ class Assistant(Agent):
 
 async def entrypoint(ctx: agents.JobContext):
     session = AgentSession(
+        stt=deepgram.STT(model="nova-3", language="multi"),
+        llm=google.LLM(model="gemini-2.5-flash"),
         stt=deepgram.STT(model="nova-3", language="multi"),
         llm=google.LLM(model="gemini-2.5-flash"),
         tts=cartesia.TTS(model="sonic-2", voice="f786b574-daa5-4673-aa0c-cbe3e8534c02"),
@@ -32,14 +35,17 @@ async def entrypoint(ctx: agents.JobContext):
         agent=Assistant(),
         room_input_options=RoomInputOptions(
             noise_cancellation=noise_cancellation.BVC(), 
+            noise_cancellation=noise_cancellation.BVC(), 
         ),
     )
 
     await ctx.connect()
 
+
     await session.generate_reply(
         instructions="Greet the user and offer your assistance."
     )
+
 
 
 if __name__ == "__main__":
